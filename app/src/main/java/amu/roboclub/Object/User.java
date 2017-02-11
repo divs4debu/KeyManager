b5e272f7898bc9ff2bfed5e74b06a3fa55692702
divyaPrakash <divs4debu@gmail.com>
2017-02-12 00:21:24 +0530

    added: User class

---
+++ app/src/main/java/amu/roboclub/Object/User.java
@@ -0,0 +1,28 @@
+package amu.roboclub.Object;
+
+/**
+ * Created by divy on 12/02/17.
+ */
+
+public class User {
+    private String name;
+    private String uid;
+
+    public User(){}
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUid() {
+        return uid;
+    }
+
+    public void setUid(String uid) {
+        this.uid = uid;
+    }
+}
