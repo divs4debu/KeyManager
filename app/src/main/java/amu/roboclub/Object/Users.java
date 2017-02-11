1efda940257225b84c480209dc1d9aca98f9ef78
divyaPrakash <divs4debu@gmail.com>
2017-02-12 00:28:27 +0530

    added: Users class,Manages User

---
+++ app/src/main/java/amu/roboclub/Object/Users.java
@@ -0,0 +1,31 @@
+package amu.roboclub.Object;
+
+import java.util.HashMap;
+
+/**
+ * Created by divy on 12/02/17.
+ */
+
+public class Users {
+    private HashMap<String, User> userMap;
+
+    public HashMap<String, User> getUserMap() {
+        return userMap;
+    }
+
+    public void setUserMap(HashMap<String, User> userMap) {
+        this.userMap = userMap;
+    }
+    public void addEntry(String uid, User user){
+        if(!isUser(uid))
+            userMap.put(uid,user);
+    }
+    public boolean isUser(String uid){
+        return userMap.containsKey(uid);
+    }
+    public User getUser(String uid){
+        if (isUser(uid))
+            return userMap.get(uid);
+        return (new User());
+    }
+}
