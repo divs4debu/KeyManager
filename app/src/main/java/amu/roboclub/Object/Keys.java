abb0dc8d623aa2b78ca72d61009f43f17d9ce89d
divyaPrakash <divs4debu@gmail.com>
2017-02-12 00:08:23 +0530

    added: Keys class to manage key

---
+++ app/src/main/java/amu/roboclub/Object/Keys.java
@@ -0,0 +1,44 @@
+package amu.roboclub.Object;
+
+/**
+ * Created by divy on 12/02/17.
+ */
+
+public class Keys {
+    private Key key1;
+    private Key key2;
+    private Key key3;
+    private Key key4;
+
+    public Key getKey1() {
+        return key1;
+    }
+
+    public void setKey1(Key key1) {
+        this.key1 = key1;
+    }
+
+    public Key getKey2() {
+        return key2;
+    }
+
+    public void setKey2(Key key2) {
+        this.key2 = key2;
+    }
+
+    public Key getKey4() {
+        return key4;
+    }
+
+    public void setKey4(Key key4) {
+        this.key4 = key4;
+    }
+
+    public Key getKey3() {
+        return key3;
+    }
+
+    public void setKey3(Key key3) {
+        this.key3 = key3;
+    }
+}
